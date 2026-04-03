d305fa7960bcf1ba02f537e3a7ea3d47eb100eab
Adam-TechStudent <akshayadam2995@gmail.com>
2026-04-03 14:28:43 -0500

    worked on the click events in content.js

--- src/Content.js
+++ src/Content.js
@@ -6,11 +6,43 @@ const Content = () => {
         return name[randomName];
     }
 
+    const handlestyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%', 
+        textAlign: 'center', 
+        fontSize: '24px', 
+        marginTop: '20px', 
+        height: '450px', 
+        backgroundColor: '#f0f0f0',
+        color: '#333',
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    }
+
+    const handleClick = () => {
+        alert('Button Clicked!');
+    }
+    const handleClick2 = (name) => {
+        alert(`Button Clicked, Hello ${name}`);
+    }
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText);
+    }
+    
+
+
+
     return (
         <main>
-            <p>
+            <p style={handlestyle}>
                 Hello {handleNameChange()} !
+                <button onClick={(handleClick)}>Click Me</button>
+                <button onClick={() => handleClick2(handleNameChange())}>Click Me</button>
+                <button onClick={(e) => handleClick3(e)}>Click Me</button>
             </p>
+            
         </main>
     )
 }
